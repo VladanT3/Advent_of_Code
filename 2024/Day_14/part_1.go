a642e854e6c74ba6b8b625d7609663e6454f6133
VladanT3 <vladt.t33@gmail.com>
2024-12-15 23:55:28 +0100

    2024 day 14 part 1 attemp, shortened and made code easier to understand for some reason still doesnt work

--- 2024/Day_14/part_1.go
+++ 2024/Day_14/part_1.go
@@ -2,7 +2,6 @@ package day14_24
 
 import (
 	"fmt"
-	"math/big"
 	"os"
 	"strconv"
 	"strings"
@@ -16,78 +15,72 @@ type coordinate struct {
 }
 
 type velocity struct {
-	vel_x int
-	vel_y int
+	x int
+	y int
+}
+
+type robot struct {
+	pos coordinate
+	vel velocity
 }
 
 func Part1() {
 	robots := GetRobots()
-	new_robots := make(map[coordinate][]velocity)
 
 	for range 100 {
-		for pos, vels := range robots {
-			for _, vel := range vels {
-				new_x := pos.x + vel.vel_x
-				new_y := pos.y + vel.vel_y
-				if new_x > 100 {
-					new_x = new_x - 100
-				} else if new_x < 0 {
-					new_x = new_x + 100
-				}
-				if new_y > 102 {
-					new_y = new_y - 102
-				} else if new_y < 0 {
-					new_y = new_y + 102
-				}
-				new_pos := coordinate{new_x, new_y}
-				if new_robots[new_pos] == nil {
-					new_robots[new_pos] = []velocity{}
-				}
-				new_robots[new_pos] = append(new_robots[new_pos], vel)
+		for _, robot := range robots {
+			robot.pos.x += robot.vel.x
+			robot.pos.y += robot.vel.y
+
+			if robot.pos.x > 100 {
+				robot.pos.x -= 100
+			} else if robot.pos.x < 0 {
+				robot.pos.x += 100
+			}
+			if robot.pos.y > 102 {
+				robot.pos.y -= 102
+			} else if robot.pos.y < 0 {
+				robot.pos.y += 102
 			}
 		}
-		robots = new_robots
-		new_robots = make(map[coordinate][]velocity)
 	}
 
-	quadrant_1 := big.NewInt(0)
-	quadrant_2 := big.NewInt(0)
-	quadrant_3 := big.NewInt(0)
-	quadrant_4 := big.NewInt(0)
-	for pos, vel := range robots {
-		if pos.x < 50 && pos.y < 51 {
-			quadrant_1 = new(big.Int).Add(quadrant_1, big.NewInt(int64(len(vel))))
+	quadrant_1 := 0
+	quadrant_2 := 0
+	quadrant_3 := 0
+	quadrant_4 := 0
+	for _, robot := range robots {
+		if robot.pos.x < 50 && robot.pos.y < 51 {
+			quadrant_1++
 		}
-		if pos.x > 50 && pos.y < 51 {
-			quadrant_2 = new(big.Int).Add(quadrant_2, big.NewInt(int64(len(vel))))
+		if robot.pos.x > 50 && robot.pos.y < 51 {
+			quadrant_2++
 		}
-		if pos.x < 50 && pos.y > 51 {
-			quadrant_3 = new(big.Int).Add(quadrant_3, big.NewInt(int64(len(vel))))
+		if robot.pos.x < 50 && robot.pos.y > 51 {
+			quadrant_3++
 		}
-		if pos.x > 50 && pos.y > 51 {
-			quadrant_4 = new(big.Int).Add(quadrant_4, big.NewInt(int64(len(vel))))
+		if robot.pos.x > 50 && robot.pos.y > 51 {
+			quadrant_4++
 		}
 	}
 
-	safety_factor := new(big.Int).Mul(
-		new(big.Int).Mul(quadrant_1, quadrant_2),
-		new(big.Int).Mul(quadrant_3, quadrant_4))
+	safety_factor := quadrant_1 * quadrant_2 * quadrant_3 * quadrant_4
 	fmt.Println(safety_factor)
 
 	out, err := os.Create("output.txt")
 	shared.ErrCheck(err)
 	defer out.Close()
 
-	_, err = out.WriteString("Part 1: " + safety_factor.String())
+	_, err = out.WriteString("Part 1: " + strconv.Itoa(safety_factor))
 	shared.ErrCheck(err)
 	out.Sync()
 }
 
-func GetRobots() map[coordinate][]velocity {
+func GetRobots() []robot {
 	f, err := os.ReadFile("input.txt")
 	shared.ErrCheck(err)
 
-	out := make(map[coordinate][]velocity)
+	out := []robot{}
 
 	rows := strings.Split(string(f), "\n")
 	for i := 0; i < len(rows)-1; i++ {
@@ -109,10 +102,7 @@ func GetRobots() map[coordinate][]velocity {
 		shared.ErrCheck(err)
 		vels := velocity{vel_x, vel_y}
 
-		if out[cord] == nil {
-			out[cord] = []velocity{}
-		}
-		out[cord] = append(out[cord], vels)
+		out = append(out, robot{cord, vels})
 	}
 
 	return out
